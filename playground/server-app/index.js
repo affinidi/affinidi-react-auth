64222c5f0fa9b89b4779cc71ffb4a47047e44cfb
Paramesh Kamarthi <paramesh.k@affinidi.com>
2023-09-29 11:20:07 +0530

    updated plugin

--- playground/server-app/index.js
+++ playground/server-app/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 require('dotenv').config()
-const affinidiProvider = require('passport-affinidi')
+const { affinidiProvider } = require('passport-affinidi')
 
 var app = express();
 app.use(express.json());
