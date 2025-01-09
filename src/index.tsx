d0110b3447e4a2d1d80c2678bd53d70e7fb8ad91
Paramesh Kamarthi <kamarthiparamesh@gmail.com>
2025-01-09 19:17:44 +0530

    fix: updated pkgs

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
-
-import AffinidiLoginButton from './LoginButton'
 import useAffinidiLogin from './hooks/useAffinidiLogin'
 import useAffinidiProfile from './hooks/useAffinidiProfile'
 import useCompleteRequest from './hooks/useCompleteRequest'
-import useInitiateRequest from './hooks/useInitiateRequest'
-import useInitiateProfileRequest from './hooks/useInitiateProfileRequest'
 import useInitiateEmailRequest from './hooks/useInitiateEmailRequest'
+import useInitiateProfileRequest from './hooks/useInitiateProfileRequest'
+import useInitiateRequest from './hooks/useInitiateRequest'
+import AffinidiLoginButton from './LoginButton'
 
-export { AffinidiLoginButton, useAffinidiLogin, useAffinidiProfile, useCompleteRequest, useInitiateRequest, useInitiateProfileRequest, useInitiateEmailRequest }
+export {
+  AffinidiLoginButton,
+  useAffinidiLogin,
+  useAffinidiProfile,
+  useCompleteRequest,
+  useInitiateRequest,
+  useInitiateProfileRequest,
+  useInitiateEmailRequest,
+}
 
 export * from './types'
